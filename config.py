98766bdafdf6996a8c7bdeb8593386df603ef029
Kalir00tKali <horrorclownDM@gmail.com>
2021-01-02 13:36:32 +0300

    first commit

---
+++ config.py
@@ -0,0 +1,4 @@
+token = "1438593342:AAFlu8AXeapDD9k7wlKsJiLy5oRYLfhgs98"
+admin_id = 768779619
+secret_code = "/liu4eg7hok"
+buyer1 = 123456789
---
+++ config_test.py
@@ -0,0 +1,15 @@
+name = "RatHouse | SHOP"
+token = "1218160376:AAGv82RYmbuGQSyxQ1HaRgyOm017V2nTRFY"
+admin_id = 768779619
+channel = "@o875463thvg4n786b"
+op = "@por0vos1k"
+token_qiwi = '71ba3311d2b7613150c6e76fbde25ceb'
+qiwi = '+79679306676'
+bitcoin = "123123123"
+logo_stick = "CAACAgIAAxkBAAISUF9mh68BTWV4PpA21ldMS_41O0hlAAJ8AAOGYFcOZkSsKfTkv0MbBA"
+secret_code = "/liu4eg7hok"
+buyer1 = 123456789
+svize = "https://t.me/por0vos1k"
+
+#if message.chat.id == config.buyer1:
+#    bot.send_message(config.buyer1, "🎉 Мои покупки: название")
